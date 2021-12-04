d0c203e0e3548f2d825a8e001896e2a13bc0f434
Daniel Nagel <danielnagel@mailbox.org>
2021-12-04 12:10:00 +0100

    Updated utils/files
     - Utility to load file names from a directory
     - Utility to check if a given path is a file

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,10 +1,33 @@
-import { existsSync, readFileSync } from "fs"
+import { existsSync, readFileSync, readdirSync, statSync } from "fs"
 
 export const fileExists = (path: string): boolean => {
     return existsSync(path);
 }
 
 export const loadFile = (path: string): string | null => {
-    if(!fileExists(path)) return null;
+    if (!fileExists(path)) return null;
     return readFileSync(path).toString();
+}
+
+export const loadFileNamesFromDirectory = (path: string, extension?: string): string[] => {
+    const fileNames: string[] = [];
+    if (fileExists(path)) {
+        const fileMetaDatas = readdirSync(path, { withFileTypes: true });
+        for (const fileMetaData of fileMetaDatas) {
+            if (!fileMetaData.isFile()) continue;
+            if(typeof extension !== "undefined") {
+                if(fileMetaData.name.endsWith(extension)) fileNames.push(fileMetaData.name);
+                continue;
+            }
+            fileNames.push(fileMetaData.name);
+        }
+    }
+    return fileNames;
+}
+
+export const isFile = (path: string): boolean => {
+    if(!fileExists(path)) return false;
+
+    const fileStat = statSync(path);
+    return fileStat.isFile();;
 }
--- tests/utils/files.test.ts
+++ tests/utils/files.test.ts
@@ -1,19 +1,39 @@
-import { fileExists, loadFile } from "../../src/utils/files";
+import { fileExists, loadFile, loadFileNamesFromDirectory, isFile } from "../../src/utils/files";
 
 describe("Test utils/files", () => {
 
     describe("Check if files exist", () => {
-        test("True if files exist", () => {
+        test("file exists", () => {
             expect(fileExists(__dirname + "/samples/sample1.txt")).toBeTruthy();
         });
-        test("False if files do not exist", () => {
+
+        test("file does not exist", () => {
             expect(fileExists(__dirname + "/samples/sample1.csv")).toBeFalsy();
         });
-        test("False if path string empty", () => {
+
+        test("path is empty", () => {
             expect(fileExists("")).toBeFalsy();
         });
+
+        test("path is directory", () => {
+            expect(fileExists(__dirname + "/samples")).toBeTruthy();
+        });
     });
-    
+
+    describe("Check if is file", () => {
+        test("file exists and is file", () => {
+            expect(isFile(__dirname + "/samples/sample1.txt")).toBeTruthy();
+        });
+
+        test("file does not exist", () => {
+            expect(isFile(__dirname + "/samples/sample1.csv")).toBeFalsy();
+        });
+
+        test("directory exists and is not file", () => {
+            expect(isFile(__dirname + "/samples")).toBeFalsy();
+        });
+    });
+
     describe("Load file", () => {
         test("Return null if file does not exist", () => {
             expect(loadFile(__dirname + "/samples/sample1.csv")).toBeNull();
@@ -21,6 +41,29 @@ describe("Test utils/files", () => {
 
         test("Load expected string from file", () => {
             expect(loadFile(__dirname + "/samples/sample1.txt")).toBe("hello world");
+        });
+    });
+
+    describe("Load files from directory", () => {
+        test("Return emptyArray if directory does not exist", () => {
+            expect(loadFileNamesFromDirectory(__dirname + "/notexisting")).toHaveLength(0);
+        });
+
+        test("Return emptyArray if directory is empty", () => {
+            expect(loadFileNamesFromDirectory(__dirname + "/empty")).toHaveLength(0);
+        });
+
+        test("Load file names from directory", () => {
+            const fileNames = loadFileNamesFromDirectory(__dirname + "/samples");
+            expect(fileNames).toHaveLength(2);
+            expect(fileNames[0]).toBe("sample1.txt");
+            expect(fileNames[1]).toBe("sample2.csv");
+        });
+
+        test("Load file names with specific file extension from directory", () => {
+            const fileNames = loadFileNamesFromDirectory(__dirname + "/samples", "csv");
+            expect(fileNames).toHaveLength(1);
+            expect(fileNames[0]).toBe("sample2.csv");
         });
     });
 
