8b3a1af87d08dccd01cffff86359c3a0b7b40563
Daniel Nagel <danielnagel@mailbox.org>
2022-05-28 14:17:49 +0200

    Initialized frontend with vite

---
+++ src/web/src/components/FixedPayDay.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const error = ref("");
+
+const setup = () => {
+    error.value = "";
+
+    // vue can't work with fs
+    // express als api server? weniger umbauen ...
+    // mit vite daten vorab laden und in die applikation einfügen
+    // theoretisch nur die config + csv.ts
+    // ist allerdings hässlich
+    console.log("wow:", config);
+};
+
+onMounted(() => {
+    setup();
+});
+</script>
+
+<template>
+    <p v-show="error">{{ error }}</p>
+</template>
